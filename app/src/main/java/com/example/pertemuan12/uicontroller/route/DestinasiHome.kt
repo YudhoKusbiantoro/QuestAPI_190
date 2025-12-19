6c2ee42fdd4f5b748cb2cbba6d0dc665e65e2993
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-12-19 21:19:26 +0700

    Membuat DestinasiHome.kt

---
+++ app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan12.uicontroller.route
+
+import com.example.pertemuan12.R
+
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
